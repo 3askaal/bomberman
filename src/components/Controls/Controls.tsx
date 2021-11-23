15f1366ae67d9613af7badacbe0944de47d9bc21
Bas Kaal <3askaal@gmail.com>
2021-11-24 00:54:38 +0100

    progress

--- src/components/Controls/Controls.tsx
+++ src/components/Controls/Controls.tsx
@@ -13,28 +13,28 @@ export const Controls = ({ onMove, onAttack, color, index }: any) => {
   return (
     <Box s={{ display: 'inline-flex', flexDirection: 'row', alignItems: ['flex-start', 'flex-end'][index] }}>
       { index === 0 ? (
-        <SControlsButton color={color} type="attack" onClick={() => onAttack()}>
+        <SControlsButton color={color} type="attack" onTouchStart={() => onAttack()} onMouseDown={() => onAttack()}>
           <Crosshair />
         </SControlsButton>
       ) : null }
       <SControls s={{ alignItems: ['flex-start', 'flex-end'][index] }}>
-        <SControlsButton color={color} type="left" onClick={() => onMove('x', -1)}>
+        <SControlsButton color={color} type="left" onTouchStart={() => onMove('x', -1)} onMouseDown={() => onMove('x', -1)}>
           <ChevronLeft />
         </SControlsButton>
         <SControlsMiddle>
-          <SControlsButton color={color} type="up" onClick={() => onMove('y', -1)}>
+          <SControlsButton color={color} type="up" onTouchStart={() => onMove('y', -1)} onMouseDown={() => onMove('y', -1)}>
             <ChevronUp />
           </SControlsButton>
-          <SControlsButton color={color} type="down" onClick={() => onMove('y', 1)}>
+          <SControlsButton color={color} type="down" onTouchStart={() => onMove('y', 1)} onMouseDown={() => onMove('y', 1)}>
             <ChevronDown />
           </SControlsButton>
         </SControlsMiddle>
-        <SControlsButton color={color} type="right" onClick={() => onMove('x', 1)}>
+        <SControlsButton color={color} type="right" onTouchStart={() => onMove('x', 1)} onMouseDown={() => onMove('x', 1)}>
           <ChevronRight />
         </SControlsButton>
       </SControls>
       { index === 1 ? (
-        <SControlsButton color={color} type="attack" onClick={() => onAttack()}>
+        <SControlsButton color={color} type="attack" onTouchStart={() => onAttack()} onMouseDown={() => onAttack()}>
           <Crosshair />
         </SControlsButton>
       ) : null }

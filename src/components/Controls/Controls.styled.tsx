31ddce4944264c9d4f10e62ba739670d296a334f
Bas Kaal <3askaal@gmail.com>
2021-11-28 21:01:22 +0100

    fix button alignment for small devices

--- src/components/Controls/Controls.styled.tsx
+++ src/components/Controls/Controls.styled.tsx
@@ -5,7 +5,7 @@ export const SControls = styled.div(({ theme }: any) => ({
   display: 'flex',
   flexDirection: 'row',
   width: '100%',
-  maxWidth: '30rem',
+  maxWidth: ['30rem', '20rem'],
   justifyContent: 'space-between'
   // maxWidth: '20rem'
   // backgroundImage: `url(${griptape})`,
@@ -16,7 +16,7 @@ export const SControlsMove = styled.div(({ theme }: any) => ({
   display: 'flex',
   position: 'relative',
   justifyContent: 'center',
-  // flexBasis: '65%',
+  flexBasis: '60%',
 
   '> *': {
     flexBasis: '33.33%'
@@ -36,16 +36,19 @@ export const SControlsButton = styled.button(({ theme, type, color }: any) => ({
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
-  height: '4rem',
+  height: ['3rem', '2rem'],
   backgroundColor: color,
   border: '.125rem solid ' + theme.colors.background,
   borderRadius: '.25rem',
   color: 'white',
-  width: '7.2rem',
+  flexBasis: '30%',
+  cursor: 'pointer',
+  // width: '100%',
+  // width: ['7.2rem', '4rem'],
 
-  // ...type !== 'attack' && ({
-  //   width: '100%',
-  // }),
+  ...type !== 'attack' && ({
+    width: '100%',
+  }),
 
   // ...type === 'attack' && ({
   //   minWidth: '8rem',

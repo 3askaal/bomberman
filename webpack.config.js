778e587f94b623435e9587068a151b200aa9866c
Bas Kaal <3askaal@gmail.com>
2021-12-05 16:27:05 +0100

    possibly fixes duplicated react instances error

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,10 @@ const path = require('path');
 module.exports = {
   resolve: {
     alias: {
-      'react': path.resolve(__dirname, 'node_modules/3oilerplate/node_modules/react'),
-      'react-dom': path.resolve(__dirname, 'node_modules/3oilerplate/node_modules/react-dom'),
-      'styled-components': path.resolve(__dirname, 'node_modules/3oilerplate/node_modules/styled-components'),
-      'styled-system': path.resolve(__dirname, 'node_modules/3oilerplate/node_modules/styled-system'),
+      // 'react': path.resolve(__dirname, 'node_modules/3oilerplate/node_modules/react'),
+      // 'react-dom': path.resolve(__dirname, 'node_modules/3oilerplate/node_modules/react-dom'),
+      // 'styled-components': path.resolve(__dirname, 'node_modules/3oilerplate/node_modules/styled-components'),
+      // 'styled-system': path.resolve(__dirname, 'node_modules/3oilerplate/node_modules/styled-system'),
     },
   },
 };

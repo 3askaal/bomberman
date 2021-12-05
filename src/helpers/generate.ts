7ee52503c9c491484e64f5a617f2520b7985026a
Bas Kaal <3askaal@gmail.com>
2021-12-05 15:38:51 +0100

    clean up

--- src/helpers/generate.ts
+++ src/helpers/generate.ts
@@ -54,68 +54,69 @@ export const generatePlayers = (blocks: number) => {
 
   return newPlayers
 }
+
 export const generateGrid = (blocks: number) => {
-    const newGrid: any = {}
-    const amountBricksForUnevenCube = (blocks * blocks) + blocks + blocks + 1
+  const newGrid: any = {}
+  const amountBricksForUnevenCube = (blocks * blocks) + blocks + blocks + 1
 
-    times(amountBricksForUnevenCube, (i) => {
-        const y = (i - (i % (blocks + 1))) / (blocks + 1)
-        const x = i % (blocks + 1)
+  times(amountBricksForUnevenCube, (i) => {
+    const y = (i - (i % (blocks + 1))) / (blocks + 1)
+    const x = i % (blocks + 1)
 
-        newGrid[`${x}/${y}`] = { x, y }
-    })
+    newGrid[`${x}/${y}`] = { x, y }
+  })
 
-    return newGrid
+  return newGrid
 }
 
 export const generateStones = (grid: any) => {
-    let newGrid = { ...grid }
+  let newGrid = { ...grid }
 
-    Object.values(grid).forEach((block: any) => {
-      const { x, y } = block
+  Object.values(grid).forEach((block: any) => {
+    const { x, y } = block
 
-      if (y % 2 && x % 2) {
-        newGrid = { ...newGrid, [`${x}/${y}`]: { ...newGrid[`${x}/${y}`], stone: true }}
-        return true
-      }
+    if (y % 2 && x % 2) {
+      newGrid = { ...newGrid, [`${x}/${y}`]: { ...newGrid[`${x}/${y}`], stone: true }}
+      return true
+    }
 
-      return false
-    })
+    return false
+  })
 
-    return newGrid
+  return newGrid
 }
 
 export const generateBricks = (grid: any, blocks: number) => {
-    let newGrid = { ...grid }
+  let newGrid = { ...grid }
 
-    const freeSpaces = Object.values(grid).filter((block: any) => {
-      const { x, y } = block
+  const freeSpaces = Object.values(grid).filter((block: any) => {
+    const { x, y } = block
 
-      const isEvenUneven = x % 2 === 1 && y % 2 === 0
-      const isUnevenEven = x % 2 === 0 && y % 2 === 1
-      const isBothUneven = x % 1 === 0 && y % 2 === 0
+    const isEvenUneven = x % 2 === 1 && y % 2 === 0
+    const isUnevenEven = x % 2 === 0 && y % 2 === 1
+    const isBothUneven = x % 1 === 0 && y % 2 === 0
 
-      if (isEvenUneven || isUnevenEven || isBothUneven) {
-        const isTopLeftCorner = y < 3 && x < 3
-        const isTopRightCorner = y < 3 && x > (blocks - 3)
-        const isBottomLeftCorner = y > (blocks - 3) && x < 3
-        const isBottomRightCorner = y > (blocks - 3) && x > (blocks - 3)
+    if (isEvenUneven || isUnevenEven || isBothUneven) {
+      const isTopLeftCorner = y < 3 && x < 3
+      const isTopRightCorner = y < 3 && x > (blocks - 3)
+      const isBottomLeftCorner = y > (blocks - 3) && x < 3
+      const isBottomRightCorner = y > (blocks - 3) && x > (blocks - 3)
 
-        const isCorner = isTopLeftCorner || isTopRightCorner || isBottomLeftCorner || isBottomRightCorner
+      const isCorner = isTopLeftCorner || isTopRightCorner || isBottomLeftCorner || isBottomRightCorner
 
-        if (!isCorner) {
-          return true
-        }
+      if (!isCorner) {
+        return true
       }
+    }
 
-      return false
-    })
+    return false
+  })
 
-    const newBricks = sampleSize(freeSpaces, (60 / 100) * freeSpaces.length)
+  const newBricks = sampleSize(freeSpaces, (60 / 100) * freeSpaces.length)
 
-    newBricks.forEach(({x, y}: any) => {
-      newGrid = { ...newGrid, [`${x}/${y}`]: { ...newGrid[`${x}/${y}`], brick: true }}
-    })
+  newBricks.forEach(({x, y}: any) => {
+    newGrid = { ...newGrid, [`${x}/${y}`]: { ...newGrid[`${x}/${y}`], brick: true }}
+  })
 
-    return newGrid
+  return newGrid
 }

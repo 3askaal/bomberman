8458ef460bf67b01f2640ba0d6b2e758326c38d1
Bas Kaal <3askaal@gmail.com>
2021-12-25 14:32:35 +0100

    attempt to fix socket production connection

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,10 +1,10 @@
 export const API_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://b0mberman-server.herokuapp.com'
+    ? 'https://b0mberman-server.herokuapp.com/'
     : 'http://localhost:1337'
 
 
 export const SOCKET_URL =
   process.env.NODE_ENV === 'production'
-    ? 'ws://b0mberman-server.herokuapp.com'
+    ? 'https://b0mberman-server.herokuapp.com/'
     : 'http://localhost:4000'

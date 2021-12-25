35d73099121ea3cf6a40ad4dfa8a862ef49dda67
Bas Kaal <3askaal@gmail.com>
2021-12-25 13:38:41 +0100

    fix socket prod url

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -6,5 +6,5 @@ export const API_URL =
 
 export const SOCKET_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://b0mberman-server.herokuapp.com'
+    ? 'https://b0mberman-server.herokuapp.com:4000'
     : 'http://localhost:4000'

c1367cbb37e6b1d664b778bfc7003bcb0643227a
Bas Kaal <3askaal@gmail.com>
2022-09-26 22:01:26 +0200

    connect to new api url

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,10 +1,4 @@
-export const API_URL =
-  process.env.NODE_ENV === 'production'
-    ? 'https://b0mberman-server.herokuapp.com/'
-    : 'http://localhost:1337'
-
-
 export const SOCKET_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://b0mberman-server.herokuapp.com/'
+    ? 'https://arcade-server.herokuapp.com/'
     : 'http://localhost:1337'
